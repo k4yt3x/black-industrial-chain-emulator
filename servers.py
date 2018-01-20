85a5663a5af470fc4b17cf204738b483497a2824
K4YT3X <k4yt3x@protonmail.com>
2018-01-20 12:25:34 -0500

    added detailed descriptions

--- servers.py
+++ servers.py
@@ -14,29 +14,43 @@ Description: Servers class initializes a list of servers
 
 class Servers:
 
-    def __init__(self, server_id, server_state):
+    def __init__(self, server_id):
         self.server_id = server_id
-        self.state = server_state
+        self.down = 0
         self.owner = 0
 
     def get_id(self):
         return self.server_id
 
-    def server_owner(self):
+    def get_server_owner(self):
+        """gets server owner id
+
+        returns current owner ID
+        if not owned by anyone (neutral), then
+        owner == 0. Otherwise owner == user ID
+        """
         return self.owner
 
     def get_availability(self):
+        """Gets server "down" information
+
+        Returns an int. in remaining "down" days
+        """
         return self.down
 
-    def get_state(self):
-        return self.state
-
-    def server_update(self):
-        if self.down > 0:
-            self.down -= 1
-
     def takeover(self, player_id):
+        """when player hacks this server
+
+        The ownership of the server will be
+        yelded to the player if this player
+        hacks it successfully
+        """
         self.owner = player_id
 
     def set_downtime(self, days):
+        """Set server downtime
+
+        When a player ddos this server or
+        if there's a conflict
+        """
         self.down = days
