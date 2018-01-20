9342ba018017eadd7da1f1ff04fe9a7e3d469af7
K4YT3X <k4yt3x@protonmail.com>
2018-01-20 02:00:57 -0500

    initializes hackers

---
+++ hacker.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Dev: K4YT3X IZAYOI
+Last Modified: 1/20/2018
+
+Dev: Thai
+Date Created: 1/20/2018
+Last Modified: 1/20/2018
+
+Description: Hackers class for game HTC
+This class defines basic attributes and actions
+for hackers.
+"""
+
+
+class hacker:
+
+    def __init__(self):
+        self.money = 5000
+        self.busy = 0  # if equals 0 then hacker can make a move
